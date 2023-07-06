4e631dd9796387e29f3219e8d279c10f5efc0a63
chikwongyip <527439024@qq.com>
2023-07-06 16:34:44 +0800

    admin ui fixed

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
   <div id="app">
-
-    <router-link to="/login">login</router-link>
-    <router-link to="/admin">admin</router-link>
     <router-view></router-view>
   </div>
 </template>
--- src/views/AdminIndex.vue
+++ src/views/AdminIndex.vue
@@ -30,6 +30,11 @@
 export default {
   // TODO:LOGO
   name: "AdminIndex",
+  data(){
+    return{
+      url:"http://localhost:8000/images/"
+    }
+  },
   methods:{
     handleMenuSelect(route){
       this.$router.push(route);
@@ -42,18 +47,13 @@ export default {
 
   .container{
     position: absolute;
-    top:80px;
-    bottom: 0px;
     width: 100%;
-    border: 1px solid red;
     .header{
       text-align:right;
       font-size:12px;
-      border:1px solid blue;
     }
     .sideBar{
       width: 200px;
-      border:1px solid green;
       background-color: rgb(238, 241, 246);
     }
     .sideBarItem{

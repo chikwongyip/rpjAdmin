4f57c6f138402534e0b1e3f181611454ef5e6848
chikwongyip <527439024@qq.com>
2023-07-18 15:23:21 +0800

    fix process environment

--- src/App.vue
+++ src/App.vue
@@ -9,18 +9,11 @@
 
 export default {
   name: 'App'
-  // components: {
-  //   HelloWorld
-  // }
 }
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  /*-webkit-font-smoothing: antialiased;*/
-  /*-moz-osx-font-smoothing: grayscale;*/
-  //text-align: center;
-  //color: #2c3e50;
 }
 </style>

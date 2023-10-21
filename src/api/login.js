71bbf63cd11934b04010509a26e4c257b4086222
chikwongyip <527439024@qq.com>
2023-10-21 13:44:19 +0800

    config ez-bio

--- src/api/login.js
+++ src/api/login.js
@@ -1,11 +1,11 @@
-import axios from 'axios'
-let base = process.env.VUE_APP_URL
+import axios from "axios";
+let base = process.env.VUE_APP_URL;
 const headers = {
-    Accept: 'application/json',
-    'Content-Type': 'application/json'
+  Accept: "application/json",
+  "Content-Type": "application/json"
 };
-export const userLogin = (params) =>{
-    return axios.post(`${base}/user/login`,params,{
-        header:headers
-    })
-}
+export const userLogin = (params) => {
+  return axios.post(`${base}/user/login`, params, {
+    header: headers
+  });
+};
